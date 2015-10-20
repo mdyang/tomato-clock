c9d99cec4ac355cd6ca2910db1c1ae636c5d9727
Mengdong Yang <conanholmes001@gmail.com>
2015-10-20 10:33:04 +0800

    VS 2013 update
    Change target framework version to 4.5, update icon, update notification
    sound, update file info (company, copyright info to 'no copyright')

--- src/TomatoClock/Properties/AssemblyInfo.cs
+++ src/TomatoClock/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TomatoClock")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EMC Corporation")]
+[assembly: AssemblyCompany("Mengdong Yang")]
 [assembly: AssemblyProduct("TomatoClock")]
-[assembly: AssemblyCopyright("Copyright © EMC Corporation 2013")]
+[assembly: AssemblyCopyright("No Copyright")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
